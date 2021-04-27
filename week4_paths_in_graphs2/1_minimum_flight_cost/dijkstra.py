120e79ab18e65104d5ec972b803599f5a238cae2
beetee17 <brandon.thio@yahoo.com.sg>
2021-04-27 16:01:05 +0800

    init

--- week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
+++ week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
@@ -1,12 +1,199 @@
+  
 #Uses python3
 
 import sys
-import queue
+from collections import defaultdict
+from queue import Queue
+import math
+
+KEY = 1
+
+class Heap():
+
+    def __init__(self, data, size, key=None):
+        self.data = data
+        self.size = size
+        self.swaps = []
+        self.key = key
+
+        if self.key:
+           self.data = [(item, key(item)) for item in self.data]
+    
+    
+    # for zero-based indexing
+    # left_child of node[i] = 2i + 1
+    # right child of node[i] = 2i + 2
+    # parent of node[i] = round_up(i/2) - 1
+
+    def get_parent(self, i):
+        index = math.ceil(i/2) - 1
+        return index if index > 0 and index < self.size else i
+
+    def get_left_child(self, i):
+        index = 2*i + 1
+        return index if index < self.size else i
+
+    def get_right_child(self, i):
+        index = 2*i + 2
+        return index if index < self.size else i
+
+    def swap(self, i, j):
+        self.data[i], self.data[j] = self.data[j], self.data[i]
+        self.swaps.append((i, j))
+
+    def sift_down(self, i):
+        # to sift a node down in min heap, check that it is strictly greater than any of its children. 
+        # If both are >, swap it with the greater of the 2 children
+        # If one of them are >, swap with that child
+        # If neither are >, do nothing
 
 
-def distance(adj, cost, s, t):
-    #write your code here
-    return -1
+        left_child_i = self.get_left_child(i)
+        right_child_i = self.get_right_child(i)
+
+        if self.key:
+
+            global KEY
+    
+            node = self.data[i][KEY]
+            left_child = self.data[left_child_i][KEY]
+            right_child = self.data[right_child_i][KEY]
+
+        else:
+            node = self.data[i]
+            left_child = self.data[left_child_i]
+            right_child = self.data[right_child_i]
+
+        if node > left_child and node > right_child:
+
+            if left_child <= right_child:
+                j =  left_child_i
+
+            else:
+                j = right_child_i
+
+            self.swap(i, j)
+            self.sift_down(j)
+        
+        elif node > left_child:
+            j = left_child_i
+            self.swap(i, j)
+            self.sift_down(j)
+        
+        elif node > right_child:
+            j = right_child_i
+            self.swap(i, j)
+            self.sift_down(j)
+
+    
+    # to build a min heap from an array, sift down all nodes from the top to second-last layer (nodes i = n/2 to 1, n = len(array))
+    def build_heap(self):
+        for i in range(self.size//2, -1, -1):
+            self.sift_down(i)
+    
+    def insert(self, element):
+        if self.key:
+            self.data = [(element, self.key(element))] + self.data
+        else:
+            self.data = [element] + self.data
+
+        self.sift_down(0)
+        self.size += 1
+
+    def extractMin(self):
+        if self.key:
+
+            min_item = self.data[0][0]
+
+        else:
+
+            min_item = self.data[0]
+
+        self.data = self.data[1:]
+        self.size -= 1
+
+        if self.size > 0:
+            self.sift_down(0)
+
+        return min_item
+
+    def isEmpty(self):
+        return self.size == 0
+
+class Vertex():
+    def __init__(self, index):
+        self.index = index
+
+        self.visited = 0
+
+        self.pre = None
+        self.post = None
+
+        # for computing of shortest path
+        self.dist = math.inf
+
+        # for computing layer of vertex in tree and retracing shortest path  
+        self.prev = None
+
+
+class Graph():
+    def __init__(self, edges, vertices):
+
+        # create a dict with vertex as key and list of its neighbours as values
+        self.adj = defaultdict(list)
+
+        self.edges = edges
+
+        # for a directed graph with edge weights, b is adjacent to a with cost of w but not vice versa
+        for ((a, b), w) in edges:
+            self.adj[vertices[a-1]].append((vertices[b-1], w))
+                    
+        self.vertices = vertices
+
+    def get_shortest_path(self, s):
+        """Given the index of a source vertex, compute the shortest path lengths of all vertices reachable from the source"""
+
+        s = self.vertices[s]
+        s.dist = 0
+
+        # use a binary min heap as priority queue for constant time extraction of min dist vertex
+        minHeap = Heap(self.vertices[:], len(self.vertices), key=lambda x: x.dist)
+
+        minHeap.build_heap()
+
+        known_region = []
+
+        # stop iterating once all vertices are of known distances
+        while len(known_region) < len(self.vertices):
+
+            u = minHeap.extractMin()
+
+            if not u in known_region:
+                known_region.append(u)
+
+            for (v, w) in self.adj[u]:
+
+                if v.dist > u.dist + w and not v in known_region:
+
+                    v.dist = u.dist + w
+
+                    # for backtracing of shortest path
+                    v.prev = u
+
+                    minHeap.insert(v)
+       
+
+
+
+
+
+def distance(graph, u, v):
+    """"Given a directed graph with positive edge weights, and the indices of 2 vertices u and v, return the length of the shortest path from u to v using dijkstra's algorithm. Return -1 if there is no path i.e. v is not reachable from u"""
+    graph.get_shortest_path(u)
+    result = graph.vertices[v].dist
+   
+
+    return result if not math.isinf(result) else -1
 
 
 if __name__ == '__main__':
@@ -14,12 +201,11 @@ if __name__ == '__main__':
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
     data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    s, t = data[0] - 1, data[1] - 1
-    print(distance(adj, cost, s, t))
+
+    u, v = data[0] - 1, data[1] - 1
+    print(distance(Graph(edges, vertices), u, v))
--- week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -1,23 +1,134 @@
 #Uses python3
 
 import sys
+from collections import defaultdict
+import math
+
+class Vertex():
+    def __init__(self, index):
+        self.index = index
+
+        self.visited = 0
+
+        # for computing of shortest path
+        self.dist = math.inf
 
 
-def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+class Graph():
+    def __init__(self, edges, vertices):
+
+        # create a dict with vertex as key and list of its neighbours as values
+        self.adj = defaultdict(list)
+
+        self.edges = edges
+                    
+        self.vertices = vertices
+        self.num_comp = 1
+        self.components = defaultdict(lambda : None)
+    
+    def explore(self, v):
+
+        # pre-vist block
+        v.visited = -1
+        
+        if not self.components[self.num_comp]:
+            self.components[self.num_comp] = v
+
+        # explore each neighbour of the vertex 
+        for neighbour in self.adj[v]:
+            if neighbour.visited == 0:
+                self.explore(neighbour)
+
+        # post-visit block
+        v.visited = 1
+            
+
+    def DFS(self):
+        
+        for v in self.vertices:
+            # explore each vertex (and its neighbours)
+            if v.visited == 0:
+                self.explore(v)
+                # once all neighbours of the vertex have been explored, they form a single connected component
+                self.num_comp += 1
+
+        self.num_comp -= 1
+
+    def bellman_ford(self, s):
+        
+        s.dist = 0
+    
+        for i in range(len(self.vertices) - 1):
+
+            no_neg_cycle = True
+
+            for ((u, v), w) in self.edges:
+
+                if v.dist > u.dist + w:
+
+                    v.dist = u.dist + w
+                    
+                    no_neg_cycle = False
+            
+            if no_neg_cycle:
+
+                break
+
+        return no_neg_cycle
+
+    def negative_cycle(self):
+        for s in self.components.values():
+            
+            no_neg_cycle = self.bellman_ford(s)
+
+            if not no_neg_cycle:
+
+                for ((u, v), w) in self.edges:
+
+                    if v.dist > u.dist + w:
+
+                        v.dist = u.dist + w
+
+                        # negative cycle found
+                        return 1
+
+        return 0
 
 
+
+
+def negative_cycle(graph):
+    """Given a directed graph with possibly negative edge weights, check whether it contains a cycle of negative weight"""
+
+    # In the problem, the question asked is if there are any negative cycles.  NOTICE that you do not have a source vertex.  The graph that will be provided may have more than one connected components--e.g., there may be no way to convert  from US Dollars to some currency.
+
+    # Does this mean that Bellman-Ford cannot be used in this situation?  Not at all. Here are a few options:
+
+    # Apply Bellman-Ford over every vertex, making it the source. This is the brute force approach. This sounds like a waste of time and it may take a long, long time in a big graph.  While this may help you pass the assignment, that's not optimal.
+
+    # Calculate the connected components and pick one vertex in each connected component and call Bellman-Ford using the vertex as the source.  This is doable.  You need to write an algorithm to implement the connected components. You can adapt one of the algorithms in the Week 1 for this
+
+    # Remember that you may have more than one connected component.  A solution is to create a dummy vertex (say v), create directed edges from v --> any other vertex with a weight of zero and now you can apply Bellman-Ford to using the newly created vertex as the source, iterating a Vth time to see if any edge is relaxed.  The reason why this works is because it allows you to connect v to any other vertex, converting the graph into a connected graph--you can reach any other vertex from the newly created vertex--with zero cost.  Algorithms in C++, 3rd Edition, and Algorithms, 4th Edition, both by Sedgwick, have this as an exercise for the reader.
+
+    # Similarly, instead of creating a new vertex, you can initialize all distances to zero instead of infinity. The purpose of using zero is--IMO--to make all vertices equidistant and when relaxing the edges the negative cycles will "pop up" in the Vth iteration of Bellman-Ford. Algorithms in C++, 3rd Edition, and Algorithms, 4th Edition, both by Sedgwick, ALSO have this as an exercise for the reader.
+    graph.DFS()
+    return graph.negative_cycle()
+ 
+
+
+
+    
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
     data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+
+    edges = [((vertices[a-1], vertices[b-1]), w) for ((a, b), w) in edges]
+
+    print(negative_cycle(Graph(edges, vertices)))
